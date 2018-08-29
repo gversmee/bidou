eeb8da8bb1969ddb79c36798e7f5a375c2b128de
Gregoire Versmee <34459351+gversmee@users.noreply.github.com>
2018-08-29 09:58:59 -0400

    Update cluster-ami-user.sh

--- cluster-ami-user.sh
+++ cluster-ami-user.sh
@@ -9,11 +9,4 @@ wget https://raw.githubusercontent.com/gversmee/bidou/master/jupyter_notebook_co
 # generate ssl certificate
 openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout /home/bidou/.jupyter/sslcert.pem -out /home/bidou/.jupyter/sslcert.pem -subj "/C=US/ST=MA/L=Boston/O=HMS/CN=."
 
-#import hail tuto
-mkdir /home/bidou/hail_tuto
-mv /opt/hail/python/hail/docs/tutorials/* /home/bidou/hail_tuto
-
-#import dcppc tuto
-git clone https://github.com/hms-dbmi/dcppc.git /home/bidou/dcppc
-
 nohup jupyter lab & > /home/bidou/.jupyter/jupyterlog.txt 2>&1
